7d1e8cb97d0a4a49e5eecaa743868e009353b53a
Joshim-Uddin <mju881@gmail.com>
2023-08-15 13:12:21 +0600

    my connections added

---
+++ app/components/suggestion.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import React from 'react';
+import image from '@/public/Ellipse.png'
+
+const Suggestion = () => {
+    return (
+        <section className='ps-3'>
+            <h3 className='md:text-2xl text-xl mt-5'>People you can also connect</h3>
+            <div className='grid md:grid-cols-3 grid-cols-1 p-3 gap-4'>
+            <div className="rounded-md border bg-base-100 shadow-xl flex justify-between items-center">
+                <div className="card-body gap-1">
+                    <h2 className="card-title">Joshim Uddin</h2>
+                    <p>Full Stack Developer</p>
+                    <p>@oruphones</p>
+                    <div className="card-actions justify-start">
+                    <button className="bg-violet-400 text-white rounded-md p-1 min:h-3 normal-case">Connect</button>
+                    </div>
+                </div>
+                
+                    <figure>
+                        <Image src={image} alt="Image"></Image>
+                    </figure>
+                
+            </div>
+            </div>
+        </section>
+    );
+};
+
+export default Suggestion;
--- app/dashboard/connections/page.jsx
+++ app/dashboard/connections/page.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import image from '@/public/Ellipse.png'
+import Image from 'next/image';
+import Suggestion from '@/app/components/suggestion';
 
 const page = () => {
     return (
-        <div className='md:absolute top-28'>
-            This is connections
+        <div className='md:absolute md:top-28 top-32 md:w-9/12 w-11/12 mx-auto'>
+            <header className='bg-[#1E2875] md:h-24 h-12 rounded-md'>
+                <h2 className='text-white md:text-3xl text-xl pt-4 ps-3'>My Connections</h2>
+            </header>
+            <section className='grid md:grid-cols-3 grid-cols-1 p-3 gap-4'>
+            <div className="rounded-md border bg-base-100 shadow-xl flex justify-between items-center">
+                <div className="card-body gap-1">
+                    <h2 className="card-title">Joshim Uddin</h2>
+                    <p>Full Stack Developer</p>
+                    <p>@oruphones</p>
+                    <div className="card-actions justify-start">
+                    <button className="bg-violet-400 text-white rounded-md p-1 min:h-3 normal-case">Remove Connection</button>
+                    </div>
+                </div>
+                
+                    <figure>
+                        <Image src={image} alt="Image"></Image>
+                    </figure>
+                
+            </div>
+            <div className="rounded-md border bg-base-100 shadow-xl flex justify-between items-center">
+                <div className="card-body gap-1">
+                    <h2 className="card-title">Joshim Uddin</h2>
+                    <p>Full Stack Developer</p>
+                    <p>@oruphones</p>
+                    <div className="card-actions justify-start">
+                    <button className="bg-violet-400 text-white rounded-md p-1 min:h-3 normal-case">Remove Connection</button>
+                    </div>
+                </div>
+                
+                    <figure>
+                        <Image src={image} alt="Image"></Image>
+                    </figure>
+                
+            </div>
+            <div className="rounded-md border bg-base-100 shadow-xl flex justify-between items-center">
+                <div className="card-body gap-1">
+                    <h2 className="card-title">Joshim Uddin</h2>
+                    <p>Full Stack Developer</p>
+                    <p>@oruphones</p>
+                    <div className="card-actions justify-start">
+                    <button className="bg-violet-400 text-white rounded-md p-1 min:h-3 normal-case">Remove Connection</button>
+                    </div>
+                </div>
+                
+                    <figure>
+                        <Image src={image} alt="Image"></Image>
+                    </figure>
+                
+            </div>
+            </section>
+            <Suggestion />
         </div>
     );
 };
