6d3aa80cfa1d29916b499b3fdf7e0ef29cdf1dc8
Joshim-Uddin <mju881@gmail.com>
2023-08-15 12:31:19 +0600

    profile page responsive done

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -12,14 +12,14 @@ const layout = ({children}) => {
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           {/* Page content here */}
-          <div className='flex items-center justify-between px-2 w-full md:border-none border-b border-gray-300 shadow-md shadow-gray-300'>
+          <div className='flex items-center justify-between px-2 w-full md:border-none border-b border-gray-300 shadow-md shadow-gray-300 mb-5'>
           <div className='flex items-center gap-3 md:hidden'>
           <label htmlFor="my-drawer-2" className="drawer-button lg:hidden"> <FaBars className='md:hidden block'/></label>
             <Image src={logo} alt='logo'></Image>
           </div>
           <div className='md:absolute top-0 right-5 md:border-b md:w-full md:shadow-md md:border-gray-400 py-2 flex items-center justify-end gap-5 md:pe-10'>
             <div><BsBell  className='text-2xl'/></div>
-            <div className='flex justify-between items-center gap-5 border py-1 px-2 rounded-md'>
+            <div className='flex justify-between items-center gap-5 md:border py-1 px-2 rounded-md'>
               
               <div className='flex items-center gap-3'>
               <Image src={profile} alt='profile image' className='rounded-md w-16'></Image>
@@ -36,13 +36,13 @@ const layout = ({children}) => {
           {children}
         
         </div> 
-        <div className="drawer-side h-fit">
+        <div className="drawer-side h-[160vh]">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
           <ul className="menu relative p-4 md:w-64 w-56 h-full bg-base-200 text-base-content">
             {/* Sidebar content here */}
             <li className='ps-8 py-2 text-xl bg-gray-50 rounded-md mb-5'>Dashboard</li>
-            <div className='flex items-center w-full mb-4'><BsChevronRight /><li className='text-[#1A1558] text-xl rounded-md w-11/12 hover:border hover:border-black'><Link href="/dashboard/profile" >My Profile</Link></li></div>
-            <div className='flex items-center w-full'><BsChevronRight /><li className='text-[#1A1558] text-xl rounded-md w-11/12 hover:border hover:border-black'><Link href="/dashboard/connections">My Connections</Link></li></div>
+            <div className='flex items-center w-full mb-4'><BsChevronRight /><li className='text-[#1A1558] md:text-xl text-base rounded-md w-11/12 hover:border hover:border-black'><Link href="/dashboard/profile" >My Profile</Link></li></div>
+            <div className='flex items-center w-full'><BsChevronRight /><li className='text-[#1A1558] md:text-xl text-base rounded-md w-11/12 hover:border hover:border-black'><Link href="/dashboard/connections">My Connections</Link></li></div>
             <div className='absolute bottom-2'>
             <button className="btn">Log Out</button>
           </div>
--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -7,51 +7,51 @@ import logo from '@/public/image12.png'
 
 const Profile = () => {
     return (
-        <main className='md:absolute top-28 w-9/12'>
-           <section className='bg-[#1E2875] rounded-md px-10 text-white md:h-56'>
+        <main className='md:absolute md:top-28 top-32 md:w-9/12 w-11/12 mx-auto'>
+           <section className='bg-[#1E2875] rounded-md px-10 text-white md:h-56 h-24'>
                 <p className='py-4'>My Profile</p>
            </section>
-           <div className='bg-white rounded-lg shadow-md md:p-8 p-4 -mt-16 w-11/12 mx-auto grid grid-cols-2 md:gap-12 gap-4'>
-             <section className='flex flex-col gap-8'>
-             <div className='flex justify-between items-center mb-10'>
+           <div className='bg-white rounded-lg shadow-md md:p-8 p-4 md:-mt-16 -mt-8 w-11/12 mx-auto grid md:grid-cols-2 grid-cols-1 md:gap-12 gap-4'>
+             <section className='flex flex-col md:gap-8 gap-3'>
+             <div className='flex justify-between items-center md:mb-10 mb-3'>
                 <Image src={Ellipse} alt="user image"></Image>
-                <button className="h-10 px-3 py-2 text-center bg-gray-200 text-md rounded-md normal-case">Upload Photo</button>
+                <button className="md:h-10 h-8 px-3 py-2 text-center bg-gray-200 md:text-base text-sm rounded-md normal-case">Upload Photo</button>
                 </div>
                 <div className='flex flex-col gap-8 border shadow-md p-5 rounded-lg'>
                     <div className='flex items-center justify-between'>
                        <div>
-                       <label htmlFor="name">Your Name</label>
-                        <p>Md. Joshim Uddin</p>
+                       <label htmlFor="name" className='text-xs md:text-base'>Your Name</label>
+                        <p className='text-sm md:text-base'>Md. Joshim Uddin</p>
                        </div>
-                       <button className="h-10 px-3 py-2 text-center bg-gray-200 text-md rounded-md normal-case">Edit</button>
+                       <button className="h-10 px-3 py-2 text-center bg-gray-200 text-base rounded-md normal-case md:block hidden">Edit</button>
                     </div>
                     <div className='flex items-end justify-between'>
                        <div>
-                       <label htmlFor="name">Your Email</label>
-                        <p>joshimuddin@gmail.com</p>
+                       <label htmlFor="name" className='text-xs md:text-base'>Your Email</label>
+                        <p className='text-sm md:text-base'>joshimuddin@gmail.com</p>
                        </div>
-                       <button className="h-10 px-3 py-2 text-center bg-gray-200 text-md rounded-md normal-case">Edit</button>
+                       <button className="h-10 px-3 py-2 text-center bg-gray-200 text-base rounded-md normal-case md:block hidden">Edit</button>
                     </div>
                     <div className='flex items-end justify-between'>
                        <div>
-                       <label htmlFor="name">Phone Number</label>
-                        <p>+8801712113334</p>
+                       <label htmlFor="name" className='text-xs md:text-base'>Phone Number</label>
+                        <p className='text-sm md:text-base'>+8801712113334</p>
                        </div>
-                       <button className="h-10 px-3 py-2 text-center bg-gray-200 text-md rounded-md normal-case">Edit</button>
+                       <button className="h-10 px-3 py-2 text-center bg-gray-200 text-base rounded-md normal-case md:block hidden">Edit</button>
                     </div>
                     
                 </div>
                 <div className='border shadow-md p-5 rounded-lg'>
-                    <div className='flex justify-between'>
-                    <h4>About <span className='text-[#413B89] font-semibold'>Joshim Uddin</span></h4>
-                    <button className="h-10 px-3 py-2 text-center bg-gray-200 text-md rounded-md normal-case">Edit</button>
+                    <div className='flex justify-between items-center'>
+                    <h4 className='md:text-base text-sm'>About <span className='text-[#413B89] font-semibold'>Joshim Uddin</span></h4>
+                    <button className="md:h-10 h-8 px-3 py-2 text-center bg-gray-200 md:text-base text-sm rounded-md normal-case">Edit</button>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium eaque, aliquid quasi quo alias quas nisi suscipit, est recusandae autem totam quis reiciendis officia cum. Impedit reprehenderit assumenda error accusantium.</p>
+                    <p className='text-justify mt-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium eaque, aliquid quasi quo alias quas nisi suscipit, est recusandae autem totam quis reiciendis officia cum. Impedit reprehenderit assumenda error accusantium.</p>
                 </div>
                 <div className='border shadow-md p-5 rounded-lg'>
                     <div className='flex justify-between items-center'>
                     <h4 className='font-semibold'>Skills</h4>
-                    <button className="h-10 px-3 py-2 text-center bg-gray-200 text-md rounded-md normal-case">Edit</button>
+                    <button className="h-10 px-3 py-2 text-center bg-gray-200 text-base rounded-md normal-case md:block hidden">Edit</button>
                     </div>
                     <ul className='flex flex-col gap-5'>
                         <li>Nextjs</li>
@@ -62,20 +62,20 @@ const Profile = () => {
              <section className='flex flex-col gap-4'>
                 <div className='flex items-center justify-between gap-10 p-4 border shadow-md rounded-md'>
                     <div>
-                    <h6 className='text-xl font-semibold mb-3'>Professional Details</h6>
-                    <p>This are the professional details shown to users in the app.</p>
+                    <h6 className='md:text-xl text-base font-semibold mb-3'>Professional Details</h6>
+                    <p className='md:text-base text-sm text-justify'>This are the professional details shown to users in the app.</p>
                     </div>
                     <div className='relative'>
-                    <FaStar className='text-6xl transform rotate-12 text-[#2684FC]'/>
-                    <FaStar className='text-4xl transform -rotate-3 text-[#413B89] absolute top-7 right-2'/>
+                    <FaStar className='md:text-6xl text-4xl transform rotate-12 text-[#2684FC]'/>
+                    <FaStar className='md:text-4xl text-2xl transform -rotate-3 text-[#413B89] absolute md:top-7 md:right-2 top-4 right-1'/>
                     </div>
                 </div>
                 <div id='certifications'>
-                    <div className='flex items-center justify-between mb-4'>
+                    <div className='flex items-center justify-between md:mb-4 mb-2'>
                     <h6 className='text-xl font-semibold mb-3'>Certifications</h6>
-                    <button className="h-10 px-3 py-2 text-center bg-gray-200 text-md rounded-md normal-case">Edit</button>
+                    <button className="h-10 px-3 py-2 text-center bg-gray-200 text-base rounded-md normal-case md:block hidden">Edit</button>
                     </div>
-                    <div className='border shadow-md rounded-full grid grid-cols-3 justify-evenly items-center p-4'>
+                    <div className='border shadow-md rounded-full grid md:grid-cols-3 grid-cols-2 justify-evenly items-center md:p-4 p-2'>
                         <div className='relative ps-5'>
                             <BiSolidPolygon className='text-6xl text-amber-400'/>
                             <FaStar className='text-xl text-white absolute top-5 left-10'/>
@@ -89,10 +89,10 @@ const Profile = () => {
                 <div id='Experience'>
                     <div className='flex items-center justify-between mb-4'>
                     <h6 className='text-xl font-semibold mb-3'>Experience</h6>
-                    <button className="h-10 px-3 py-2 text-center bg-gray-200 text-md rounded-md normal-case">Edit</button>
+                    <button className="h-10 px-3 py-2 text-center bg-gray-200 text-base rounded-md normal-case md:block hidden">Edit</button>
                     </div>
-                    <div className='flex flex-col gap-4'> 
-                    <div className='border shadow-md rounded-md flex gap-4 items-center p-4 relative'>
+                    <div className='flex flex-col md:gap-4 gap-2'> 
+                    <div className='border shadow-md rounded-md flex md:flex-row flex-col gap-4 items-center md:p-4 p-2 relative'>
                         
                         <div>
                             <p className='text-sm font-semibold text-center'>7 years (2014-21) </p>
@@ -102,9 +102,9 @@ const Profile = () => {
                             <p className='text-sm font-semibold text-center'>Full Time </p>
                             <p className='text-sm  text-center'>-- Full Stack Developer </p>
                         </div>                            
-                        <Image src={logo} alt='logo' className='absolute right-3'></Image> 
+                        <Image src={logo} alt='logo' className='md:absolute md:right-3'></Image> 
                     </div>
-                    <div className='border shadow-md rounded-md flex gap-4 items-center p-4 relative'>
+                    <div className='border shadow-md rounded-md flex md:flex-row flex-col gap-4 items-center p-4 relative'>
                         
                         <div>
                             <p className='text-sm font-semibold text-center'>6 months (2014) </p>
@@ -114,7 +114,7 @@ const Profile = () => {
                             <p className='text-sm font-semibold text-center'>Intern </p>
                             <p className='text-sm  text-center'>-- Full Stack Developer </p>
                         </div>                            
-                        <Image src={logo} alt='logo' className='absolute right-3'></Image> 
+                        <Image src={logo} alt='logo' className='md:absolute right-3'></Image> 
                     </div>
                     </div>
                     
@@ -122,7 +122,7 @@ const Profile = () => {
                 <div id='education'>
                     <div className='flex items-center justify-between py-5 mb-4'>
                     <h6 className='text-xl font-semibold'>Education</h6>
-                    <button className="h-10 px-3 py-2 text-center bg-gray-200 text-md rounded-md normal-case">Edit</button>
+                    <button className="h-10 px-3 py-2 text-center bg-gray-200 text-base rounded-md normal-case md:block hidden">Edit</button>
                     </div>
                     <div className='border shadow-md rounded-md p-4'>
                         <div className='relative ps-5'>
